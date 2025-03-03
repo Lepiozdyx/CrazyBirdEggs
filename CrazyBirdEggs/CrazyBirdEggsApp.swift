04775cb38ff257e4340ffdd925aa5b534cded353
Developer <Lepiozdyx@gmail.com>
2025-03-03 10:51:26 -0300

    Initial Commit

---
+++ CrazyBirdEggs/CrazyBirdEggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyBirdEggsApp.swift
+//  CrazyBirdEggs
+//
+//  Created by Alex on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CrazyBirdEggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
